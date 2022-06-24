3e74f070869c0c7df55fc9334b992f136d4a6dba
Joe Williams <jwilliams@joinpapa.com>
2022-06-24 16:23:12 -0400

    new stuff

--- nvim/init.lua
+++ nvim/init.lua
@@ -34,12 +34,16 @@ plug 'ryanoasis/vim-devicons'
 plug 'mhinz/vim-mix-format'
 
 plug 'airblade/vim-gitgutter'
+plug 'plasticboy/vim-markdown'
 
 -- Nightlys
 plug {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}
 plug 'nvim-treesitter/playground'
+plug "williamboman/nvim-lsp-installer"
 plug 'neovim/nvim-lspconfig'
 plug 'nvim-lua/plenary.nvim'
+plug 'jose-elias-alvarez/null-ls.nvim'
+plug 'jose-elias-alvarez/nvim-lsp-ts-utils'
 plug 'nvim-telescope/telescope.nvim'
 plug 'hrsh7th/cmp-nvim-lsp'
 plug 'hrsh7th/cmp-buffer'
@@ -67,8 +71,8 @@ vim.opt.encoding='utf-8'
 vim.opt.number=true
 vim.opt.background='dark'
 cmd('syntax on')
-cmd('colorscheme github')
---cmd('colorscheme palenight')
+--cmd('colorscheme github')
+cmd('colorscheme palenight')
 --cmd('colorscheme horizon')
 
 
@@ -114,6 +118,7 @@ map('n', '<leader>ub',   ':UltiSnipsJumpBackwardTrigger',{noremap=true})
 
 -- If you want :UltiSnipsEdit to split your window.
 vim.g.ultisnips_edit_split="vertical"
+vim.g.UltiSnipsExpandTrigger=",uex"
 
 require('telescope').setup{
   defaults = {
@@ -195,21 +200,181 @@ require('telescope').setup{
   })
 
 
+  local null_ls = require("null-ls")
+
+  null_ls.setup({
+    null_ls.builtins.diagnostics.credo,
+    null_ls.builtins.diagnostics.eslint,
+  })
+
+
   -- Setup lspconfig.
   local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
   -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+  require("nvim-lsp-installer").setup( {
+    -- A list of servers to automatically install if they're not already installed. Example: { "rust_analyzer", "sumneko_lua" }
+    -- This setting has no relation with the `automatic_installation` setting.
+    ensure_installed = {},
 
-require'lspconfig'.elixirls.setup{
-  cmd = {"/Users/jwilliams/language_server.sh"},
-  capabilities = capabilities
+    -- Whether servers that are set up (via lspconfig) should be automatically installed if they're not already installed.
+    -- This setting has no relation with the `ensure_installed` setting.
+    -- Can either be:
+    --   - false: Servers are not automatically installed.
+    --   - true: All servers set up via lspconfig are automatically installed.
+    --   - { exclude: string[] }: All servers set up via lspconfig, except the ones provided in the list, are automatically installed.
+    --       Example: automatic_installation = { exclude = { "rust_analyzer", "solargraph" } }
+    automatic_installation = false,
+
+    ui = {
+        -- Whether to automatically check for outdated servers when opening the UI window.
+        check_outdated_servers_on_open = true,
+
+        -- The border to use for the UI window. Accepts same border values as |nvim_open_win()|.
+        border = "none",
+
+        icons = {
+            -- The list icon to use for installed servers.
+            server_installed = "◍",
+            -- The list icon to use for servers that are pending installation.
+            server_pending = "◍",
+            -- The list icon to use for servers that are not installed.
+            server_uninstalled = "◍",
+        },
+        keymaps = {
+            -- Keymap to expand a server in the UI
+            toggle_server_expand = "<CR>",
+            -- Keymap to install the server under the current cursor position
+            install_server = "i",
+            -- Keymap to reinstall/update the server under the current cursor position
+            update_server = "u",
+            -- Keymap to check for new version for the server under the current cursor position
+            check_server_version = "c",
+            -- Keymap to update all installed servers
+            update_all_servers = "U",
+            -- Keymap to check which installed servers are outdated
+            check_outdated_servers = "C",
+            -- Keymap to uninstall a server
+            uninstall_server = "X",
+        },
+    },
+
+    -- The directory in which to install all servers.
+    install_root_dir = "~/lsp_servers",
+
+    pip = {
+        -- These args will be added to `pip install` calls. Note that setting extra args might impact intended behavior
+        -- and is not recommended.
+        --
+        -- Example: { "--proxy", "https://proxyserver" }
+        install_args = {},
+    },
+
+    -- Controls to which degree logs are written to the log file. It's useful to set this to vim.log.levels.DEBUG when
+    -- debugging issues with server installations.
+    log_level = vim.log.levels.INFO,
+
+    -- Limit for the maximum amount of servers to be installed at the same time. Once this limit is reached, any further
+    -- servers that are requested to be installed will be put in a queue.
+    max_concurrent_installers = 4,
+
+    github = {
+        -- The template URL to use when downloading assets from GitHub.
+        -- The placeholders are the following (in order):
+        -- 1. The repository (e.g. "rust-lang/rust-analyzer")
+        -- 2. The release version (e.g. "v0.3.0")
+        -- 3. The asset name (e.g. "rust-analyzer-v0.3.0-x86_64-unknown-linux-gnu.tar.gz")
+        download_url_template = "https://github.com/%s/releases/download/%s/%s",
+    },
 }
+  )
 
+local lsp = require'lspconfig'
+
+  --
+-- JAVASCRIPT
+--
+lsp.tsserver.setup({
+    -- Needed for inlayHints. Merge this table with your settings or copy
+    -- it from the source if you want to add your own init_options.
+    init_options = require("nvim-lsp-ts-utils").init_options,
+    --
+    on_attach = function(client, bufnr)
+        local ts_utils = require("nvim-lsp-ts-utils")
+
+        -- defaults
+        ts_utils.setup({
+            debug = false,
+            disable_commands = false,
+            enable_import_on_completion = false,
+
+            -- import all
+            import_all_timeout = 5000, -- ms
+            -- lower numbers = higher priority
+            import_all_priorities = {
+                same_file = 1, -- add to existing import statement
+                local_files = 2, -- git files or files with relative path markers
+                buffer_content = 3, -- loaded buffer content
+                buffers = 4 -- loaded buffer names
+            },
+            import_all_scan_buffers = 100,
+            import_all_select_source = false,
+            -- if false will avoid organizing imports
+            always_organize_imports = true,
+
+            -- filter diagnostics
+            filter_out_diagnostics_by_severity = {},
+            filter_out_diagnostics_by_code = {},
+
+            -- inlay hints
+            auto_inlay_hints = true,
+            inlay_hints_highlight = "Comment",
+            inlay_hints_priority = 200, -- priority of the hint extmarks
+            inlay_hints_throttle = 150, -- throttle the inlay hint request
+            inlay_hints_format = { -- format options for individual hint kind
+                Type = {},
+                Parameter = {},
+                Enum = {}
+                -- Example format customization for `Type` kind:
+                -- Type = {
+                --     highlight = "Comment",
+                --     text = function(text)
+                --         return "->" .. text:sub(2)
+                --     end,
+                -- },
+            },
+
+            -- update imports on file move
+            update_imports_on_move = false,
+            require_confirmation_on_move = false,
+            watch_dir = nil
+        })
+
+        -- required to fix code action ranges and filter diagnostics
+        ts_utils.setup_client(client)
+
+        -- no default maps, so you may want to define some here
+        local opts = {silent = true}
+        vim.api
+            .nvim_buf_set_keymap(bufnr, "n", "gs", ":TSLspOrganize<CR>", opts)
+        vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", ":TSLspRenameFile<CR>",
+                                    opts)
+        vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", ":TSLspImportAll<CR>",
+                                    opts)
+    end
+})
+
+lsp.elixirls.setup{
+  cmd = {"~/lsp_servers/elixirls/language_server.sh"},
+  capabilities = capabilites
+}
 
 
 
 --:filetype plugin on
 vim.g.gitgutter_realtime=0
 vim.g.gitgutter_eager=0
+
+vim.g.LANG=en_us
 
 
 vim.g.mix_format_on_save = 1
